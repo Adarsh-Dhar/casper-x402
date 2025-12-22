a10d1c5db420ed72aab7cfc58c2ccc74075dfe1b
Adarsh Dhar <dharadarsh0@gmail.com>
2025-12-22 14:14:15 +0530

    clean

--- facilitator/test-setup.js
+++
@@ -1,7 +0,0 @@
-// Test setup file
-// Set test environment variables
-process.env.NODE_ENV = 'test';
-process.env.PORT = '0'; // Use random available port for tests
-
-// Increase timeout for property-based tests
-jest.setTimeout(30000);
